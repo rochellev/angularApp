be397ad2e447457f0980d14827f0b65f5f1c2e03
rochellev <rochellevroberts@gmail.com>
2018-03-30 13:01:13 -0700

    services to update data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -17,7 +16,8 @@ export class HeroesComponent implements OnInit {
   constructor(private heroService: HeroService) { }
  
  getHeroes(): void {
-  this.heroes = this.heroService.getHeroes();
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
 }
 
   ngOnInit() {
